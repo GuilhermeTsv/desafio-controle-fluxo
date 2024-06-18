e30314718983471ac6d9abdda3b8458c50a2dae4
GuilhermeTsv <guilhermeteixeira2007@outlook.com>
2024-06-18 20:07:32 -0300

    commit

--- src/Contador.java
+++ src/Contador.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 
-// Definição da exceção personalizada ParametrosInvalidosException
-
-
 public class Contador {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -12,11 +9,10 @@ public class Contador {
         int parametroDois = scan.nextInt();
 
         try {
-            // Chamando o método contendo a lógica de contagem
+
             contar(parametroUm, parametroDois);
 
         } catch (ParametrosInvalidosException e) {
-            // Imprimir a mensagem de exceção capturada
             System.out.println("O segundo parâmetro precisa ser maior que o primeiro");
         }
 
@@ -24,13 +20,12 @@ public class Contador {
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção, se necessário
+
         if (parametroUm >= parametroDois) {
             throw new ParametrosInvalidosException();
         }
 
         int contagem = parametroUm;
-        // Realizar o for para imprimir os números com base na variável contagem
         for (int i = parametroUm; i < parametroDois; i++) {
             System.out.println(contagem);
             contagem++;
